c34c204cb6fe16a6d6c054d54f0c1c38cfea2fe5
Jakub Petrik <jakub.petrik@automatic.com>
2020-04-14 09:28:45 +0200

    Update Package.swift
    Summary:
    - Specify platforms and Swift versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "Animalese-Swift",
+    platforms: [
+        .iOS(.v12),
+        .watchOS(.v5),
+        .tvOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -21,5 +26,6 @@ let package = Package(
         .target(
             name: "Animalese-Swift",
             dependencies: [])
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
